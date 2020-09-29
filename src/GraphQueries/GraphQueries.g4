ac4a8c14c104b7a489d507fa0475fd5a44800f38
Ruslana Tsehanovskaya <lanasheep@yandex.ru>
2020-09-29 20:42:00 +0300

    add antlr parser

---
+++ src/GraphQueries/GraphQueries.g4
@@ -0,0 +1,86 @@
+grammar GraphQueries;
+
+script : (stmt SEMI)* EOF ;
+
+stmt : KW_CONNECT KW_TO STRING
+     | KW_LIST
+     | select_stmt
+     | named_pattern
+     ;
+
+named_pattern : NT_NAME OP_EQ pattern ;
+
+select_stmt : KW_SELECT obj_expr KW_FROM STRING KW_WHERE where_expr ;
+
+obj_expr : vs_info
+         | KW_COUNT vs_info
+         | KW_EXISTS vs_info
+         ;
+
+vs_info : LBR v_info COMMA v_info RBR
+        | v_info
+        ;
+
+v_info : SYMB
+       | UNDERSCORE
+       ;
+
+where_expr : LBR v_expr RBR OP_MINUS pattern OP_MINUS OP_GR LBR v_expr RBR ;
+
+v_expr : SYMB
+       | UNDERSCORE
+       | SYMB DOT KW_ID OP_EQ INT
+       ;
+
+pattern : elem
+        | elem MID pattern
+        ;
+
+elem : seq
+     | LBR RBR
+     ;
+
+seq : seq_elem
+    | seq_elem seq
+    ;
+
+seq_elem : prim_pattern
+         | prim_pattern OP_STAR
+         | prim_pattern OP_PLUS
+         | prim_pattern OP_Q
+         ;
+
+prim_pattern : SYMB
+             | NT_NAME
+             | LBR pattern RBR
+             ;
+
+LBR : '(' ;
+RBR : ')' ;
+COMMA : ',' ;
+SEMI : ';' ;
+MID : '|' ;
+DOT : '.' ;
+UNDERSCORE : '_' ;
+OP_STAR : '*' ;
+OP_PLUS : '+' ;
+OP_Q : '?' ;
+OP_MINUS : '-' ;
+OP_GR : '>' ;
+OP_EQ : '=' ;
+KW_ID : 'id' ;
+KW_SELECT : 'select' ;
+KW_COUNT : 'count' ;
+KW_EXISTS : 'exists' ;
+KW_FROM : 'from' ;
+KW_WHERE : 'where' ;
+KW_LIST : 'list' ;
+KW_CONNECT : 'connect' ;
+KW_TO : 'to' ;
+INT : '0'
+    | [1−9][0−9]*
+    ;
+SYMB : [a-z]+ ;
+NT_NAME : [A-Z]+ ;
+STRING : '[' ([a-zA-Z]|[0-9]|('-' | '_' | ' ' | '/' |'.' | ','))* ']' ;
+WS : [ \r\n\t]+ -> skip ;
---
+++ src/GraphQueries/GraphQueriesLexer.py
@@ -0,0 +1,144 @@
+# Generated from GraphQueries.g4 by ANTLR 4.8
+from antlr4 import *
+from io import StringIO
+from typing.io import TextIO
+import sys
+
+
+
+def serializedATN():
+    with StringIO() as buf:
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\35")
+        buf.write("\u00a9\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7")
+        buf.write("\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t\r")
+        buf.write("\4\16\t\16\4\17\t\17\4\20\t\20\4\21\t\21\4\22\t\22\4\23")
+        buf.write("\t\23\4\24\t\24\4\25\t\25\4\26\t\26\4\27\t\27\4\30\t\30")
+        buf.write("\4\31\t\31\4\32\t\32\4\33\t\33\4\34\t\34\3\2\3\2\3\3\3")
+        buf.write("\3\3\4\3\4\3\5\3\5\3\6\3\6\3\7\3\7\3\b\3\b\3\t\3\t\3\n")
+        buf.write("\3\n\3\13\3\13\3\f\3\f\3\r\3\r\3\16\3\16\3\17\3\17\3\17")
+        buf.write("\3\20\3\20\3\20\3\20\3\20\3\20\3\20\3\21\3\21\3\21\3\21")
+        buf.write("\3\21\3\21\3\22\3\22\3\22\3\22\3\22\3\22\3\22\3\23\3\23")
+        buf.write("\3\23\3\23\3\23\3\24\3\24\3\24\3\24\3\24\3\24\3\25\3\25")
+        buf.write("\3\25\3\25\3\25\3\26\3\26\3\26\3\26\3\26\3\26\3\26\3\26")
+        buf.write("\3\27\3\27\3\27\3\30\3\30\3\30\7\30\u0089\n\30\f\30\16")
+        buf.write("\30\u008c\13\30\5\30\u008e\n\30\3\31\6\31\u0091\n\31\r")
+        buf.write("\31\16\31\u0092\3\32\6\32\u0096\n\32\r\32\16\32\u0097")
+        buf.write("\3\33\3\33\7\33\u009c\n\33\f\33\16\33\u009f\13\33\3\33")
+        buf.write("\3\33\3\34\6\34\u00a4\n\34\r\34\16\34\u00a5\3\34\3\34")
+        buf.write("\2\2\35\3\3\5\4\7\5\t\6\13\7\r\b\17\t\21\n\23\13\25\f")
+        buf.write("\27\r\31\16\33\17\35\20\37\21!\22#\23%\24\'\25)\26+\27")
+        buf.write("-\30/\31\61\32\63\33\65\34\67\35\3\2\b\7\2\63\63;;\u0434")
+        buf.write("\u0434\u201b\u201b\u20ae\u20ae\7\2\62\62;;\u0434\u0434")
+        buf.write("\u201b\u201b\u20ae\u20ae\3\2c|\3\2C\\\7\2\"\".;C\\aac")
+        buf.write("|\5\2\13\f\17\17\"\"\2\u00ae\2\3\3\2\2\2\2\5\3\2\2\2\2")
+        buf.write("\7\3\2\2\2\2\t\3\2\2\2\2\13\3\2\2\2\2\r\3\2\2\2\2\17\3")
+        buf.write("\2\2\2\2\21\3\2\2\2\2\23\3\2\2\2\2\25\3\2\2\2\2\27\3\2")
+        buf.write("\2\2\2\31\3\2\2\2\2\33\3\2\2\2\2\35\3\2\2\2\2\37\3\2\2")
+        buf.write("\2\2!\3\2\2\2\2#\3\2\2\2\2%\3\2\2\2\2\'\3\2\2\2\2)\3\2")
+        buf.write("\2\2\2+\3\2\2\2\2-\3\2\2\2\2/\3\2\2\2\2\61\3\2\2\2\2\63")
+        buf.write("\3\2\2\2\2\65\3\2\2\2\2\67\3\2\2\2\39\3\2\2\2\5;\3\2\2")
+        buf.write("\2\7=\3\2\2\2\t?\3\2\2\2\13A\3\2\2\2\rC\3\2\2\2\17E\3")
+        buf.write("\2\2\2\21G\3\2\2\2\23I\3\2\2\2\25K\3\2\2\2\27M\3\2\2\2")
+        buf.write("\31O\3\2\2\2\33Q\3\2\2\2\35S\3\2\2\2\37V\3\2\2\2!]\3\2")
+        buf.write("\2\2#c\3\2\2\2%j\3\2\2\2\'o\3\2\2\2)u\3\2\2\2+z\3\2\2")
+        buf.write("\2-\u0082\3\2\2\2/\u008d\3\2\2\2\61\u0090\3\2\2\2\63\u0095")
+        buf.write("\3\2\2\2\65\u0099\3\2\2\2\67\u00a3\3\2\2\29:\7*\2\2:\4")
+        buf.write("\3\2\2\2;<\7+\2\2<\6\3\2\2\2=>\7.\2\2>\b\3\2\2\2?@\7=")
+        buf.write("\2\2@\n\3\2\2\2AB\7~\2\2B\f\3\2\2\2CD\7\60\2\2D\16\3\2")
+        buf.write("\2\2EF\7a\2\2F\20\3\2\2\2GH\7,\2\2H\22\3\2\2\2IJ\7-\2")
+        buf.write("\2J\24\3\2\2\2KL\7A\2\2L\26\3\2\2\2MN\7/\2\2N\30\3\2\2")
+        buf.write("\2OP\7@\2\2P\32\3\2\2\2QR\7?\2\2R\34\3\2\2\2ST\7k\2\2")
+        buf.write("TU\7f\2\2U\36\3\2\2\2VW\7u\2\2WX\7g\2\2XY\7n\2\2YZ\7g")
+        buf.write("\2\2Z[\7e\2\2[\\\7v\2\2\\ \3\2\2\2]^\7e\2\2^_\7q\2\2_")
+        buf.write("`\7w\2\2`a\7p\2\2ab\7v\2\2b\"\3\2\2\2cd\7g\2\2de\7z\2")
+        buf.write("\2ef\7k\2\2fg\7u\2\2gh\7v\2\2hi\7u\2\2i$\3\2\2\2jk\7h")
+        buf.write("\2\2kl\7t\2\2lm\7q\2\2mn\7o\2\2n&\3\2\2\2op\7y\2\2pq\7")
+        buf.write("j\2\2qr\7g\2\2rs\7t\2\2st\7g\2\2t(\3\2\2\2uv\7n\2\2vw")
+        buf.write("\7k\2\2wx\7u\2\2xy\7v\2\2y*\3\2\2\2z{\7e\2\2{|\7q\2\2")
+        buf.write("|}\7p\2\2}~\7p\2\2~\177\7g\2\2\177\u0080\7e\2\2\u0080")
+        buf.write("\u0081\7v\2\2\u0081,\3\2\2\2\u0082\u0083\7v\2\2\u0083")
+        buf.write("\u0084\7q\2\2\u0084.\3\2\2\2\u0085\u008e\7\62\2\2\u0086")
+        buf.write("\u008a\t\2\2\2\u0087\u0089\t\3\2\2\u0088\u0087\3\2\2\2")
+        buf.write("\u0089\u008c\3\2\2\2\u008a\u0088\3\2\2\2\u008a\u008b\3")
+        buf.write("\2\2\2\u008b\u008e\3\2\2\2\u008c\u008a\3\2\2\2\u008d\u0085")
+        buf.write("\3\2\2\2\u008d\u0086\3\2\2\2\u008e\60\3\2\2\2\u008f\u0091")
+        buf.write("\t\4\2\2\u0090\u008f\3\2\2\2\u0091\u0092\3\2\2\2\u0092")
+        buf.write("\u0090\3\2\2\2\u0092\u0093\3\2\2\2\u0093\62\3\2\2\2\u0094")
+        buf.write("\u0096\t\5\2\2\u0095\u0094\3\2\2\2\u0096\u0097\3\2\2\2")
+        buf.write("\u0097\u0095\3\2\2\2\u0097\u0098\3\2\2\2\u0098\64\3\2")
+        buf.write("\2\2\u0099\u009d\7]\2\2\u009a\u009c\t\6\2\2\u009b\u009a")
+        buf.write("\3\2\2\2\u009c\u009f\3\2\2\2\u009d\u009b\3\2\2\2\u009d")
+        buf.write("\u009e\3\2\2\2\u009e\u00a0\3\2\2\2\u009f\u009d\3\2\2\2")
+        buf.write("\u00a0\u00a1\7_\2\2\u00a1\66\3\2\2\2\u00a2\u00a4\t\7\2")
+        buf.write("\2\u00a3\u00a2\3\2\2\2\u00a4\u00a5\3\2\2\2\u00a5\u00a3")
+        buf.write("\3\2\2\2\u00a5\u00a6\3\2\2\2\u00a6\u00a7\3\2\2\2\u00a7")
+        buf.write("\u00a8\b\34\2\2\u00a88\3\2\2\2\n\2\u008a\u008d\u0092\u0097")
+        buf.write("\u009b\u009d\u00a5\3\b\2\2")
+        return buf.getvalue()
+
+
+class GraphQueriesLexer(Lexer):
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    LBR = 1
+    RBR = 2
+    COMMA = 3
+    SEMI = 4
+    MID = 5
+    DOT = 6
+    UNDERSCORE = 7
+    OP_STAR = 8
+    OP_PLUS = 9
+    OP_Q = 10
+    OP_MINUS = 11
+    OP_GR = 12
+    OP_EQ = 13
+    KW_ID = 14
+    KW_SELECT = 15
+    KW_COUNT = 16
+    KW_EXISTS = 17
+    KW_FROM = 18
+    KW_WHERE = 19
+    KW_LIST = 20
+    KW_CONNECT = 21
+    KW_TO = 22
+    INT = 23
+    SYMB = 24
+    NT_NAME = 25
+    STRING = 26
+    WS = 27
+
+    channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN" ]
+
+    modeNames = [ "DEFAULT_MODE" ]
+
+    literalNames = [ "<INVALID>",
+            "'('", "')'", "','", "';'", "'|'", "'.'", "'_'", "'*'", "'+'", 
+            "'?'", "'-'", "'>'", "'='", "'id'", "'select'", "'count'", "'exists'", 
+            "'from'", "'where'", "'list'", "'connect'", "'to'" ]
+
+    symbolicNames = [ "<INVALID>",
+            "LBR", "RBR", "COMMA", "SEMI", "MID", "DOT", "UNDERSCORE", "OP_STAR", 
+            "OP_PLUS", "OP_Q", "OP_MINUS", "OP_GR", "OP_EQ", "KW_ID", "KW_SELECT", 
+            "KW_COUNT", "KW_EXISTS", "KW_FROM", "KW_WHERE", "KW_LIST", "KW_CONNECT", 
+            "KW_TO", "INT", "SYMB", "NT_NAME", "STRING", "WS" ]
+
+    ruleNames = [ "LBR", "RBR", "COMMA", "SEMI", "MID", "DOT", "UNDERSCORE", 
+                  "OP_STAR", "OP_PLUS", "OP_Q", "OP_MINUS", "OP_GR", "OP_EQ", 
+                  "KW_ID", "KW_SELECT", "KW_COUNT", "KW_EXISTS", "KW_FROM", 
+                  "KW_WHERE", "KW_LIST", "KW_CONNECT", "KW_TO", "INT", "SYMB", 
+                  "NT_NAME", "STRING", "WS" ]
+
+    grammarFileName = "GraphQueries.g4"
+
+    def __init__(self, input=None, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.8")
+        self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
+        self._actions = None
+        self._predicates = None
+
+
